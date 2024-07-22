24b94f535848c024139582eba1ffe002972d5c8f
[Mikiyas] <[mikasol9134@gmail.com]>
2024-07-22 04:12:52 -0400

    initing the express server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.get ('/', (req, res)=> {
+    res.render("home.ejs")
+})
+app.listen(3000,()=> { 
+    console.log("Listinig on PORT 3000")
+})
