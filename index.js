c5ac8b584c5ff389984ca22ea079d3e4267eb9b2
[Mikiyas] <[mikasol9134@gmail.com]>
2024-07-22 06:29:40 -0400

    updtaes on file

--- index.js
+++ index.js
@@ -1,10 +1,71 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// //const { request } = require('http');
+// const app = express();
+// const path =request('path');
 
-app.set('view engine', 'ejs');
-app.get ('/', (req, res)=> {
-    res.render("home.ejs")
-})
+// app.set('views',path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+
+// const comments = [
+//     {
+//         usernmae: 'todd',
+//         comment:'haha realy funny'
+//     },
+//     {
+//         usernmae: 'miki',
+//         comment:'my cat loves this shit '
+//     },
+//     {
+//         usernmae: 'abel',
+//         comment:'haha realy pls, delet ur acc'
+//     },
+//     {
+//         usernmae: 'totolosadd',
+//         comment:'useless and un interesting content'
+//     },
+// ]
+
+// app.get ('/comments', (req, res) => {
+//     res.render('comments/index')
+
+// } )
+
+
+// app.listen(3000,()=> { 
+//     console.log("Listinig on PORT 3000")
+
+const express = require('express');
+const path = require ('path');
+
+const app = express();
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+
+const comments = [
+    {
+        usernmae: 'todd',
+        comment:'haha realy funny'
+    },
+    {
+        usernmae: 'miki',
+        comment:'my cat loves this shit '
+    },
+    {
+        usernmae: 'abel',
+        comment:'haha realy pls, delete ur acc'
+    },
+    {
+        usernmae: 'totolosadd',
+        comment:'useless and un interesting content'
+    },
+]
+
+app.get('/comments', (req, res) => {
+    res.render('comments/index')
+
+} )
+
 app.listen(3000,()=> { 
     console.log("Listinig on PORT 3000")
 })
